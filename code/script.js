d3993141bbfceb1f300d6dbfc9c6ec6e03a15b50
motherofbeasts <91845855+motherofbeasts@users.noreply.github.com>
2022-02-20 22:09:58 +0100

    started to add a search function

---
+++ code/script.js
@@ -0,0 +1,17 @@
+/*search function 
+const api
+let repo = 'Weather-app'
+let apiKey = '&APPID'
+const units = '&units=metric';
+
+const url = api + repo + apiKey + units
+
+const button = select ('#submit');
+button.mousePressed (askForRepo);
+
+function askForRepo (){
+    const url = api + apiKey + units;
+    LoadJson (url,gotData); InputEvent.value ()
+
+    let input;
+} */
